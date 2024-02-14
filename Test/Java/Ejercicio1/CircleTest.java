a47b8878c1f999bccd115a38d5a49621a46f27b6
UAH <martavalientemaldonado@gmail.com>
2024-02-14 09:42:38 +0100

    arreglos proyecto

---
+++ Test/Java/Ejercicio1/CircleTest.java
@@ -0,0 +1,30 @@
+package Ejercicio1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircleTest {
+
+    @org.junit.jupiter.api.Test
+    void getRadius() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getColor() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setRadius() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setColor() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getArea() {
+    }
+}
