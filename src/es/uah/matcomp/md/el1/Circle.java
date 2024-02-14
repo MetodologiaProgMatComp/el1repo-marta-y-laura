e7c9f1db2b85f8dd6c983dbec257711d067c7989
UAH <martavalientemaldonado@gmail.com>
2024-02-14 08:52:18 +0100

    arreglos proyecto

---
+++ src/es/uah/matcomp/md/el1/Circle.java
@@ -0,0 +1,41 @@
+package es.uah.matcomp.md.el1;
+
+public class Circle {
+    private double radius;
+    private String color;
+    public Circle (){
+        radius = 1.0;
+        color = "red";
+    }
+    public Circle(double r) { // 2nd constructor
+        radius = r;
+        color = "red";
+    }
+    public Circle (double r, String color){
+        radius = r;
+        color = "red";
+
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public String getColor(){
+        return color;
+
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public String toString(){
+        return "es.uah.matcomp.md.el1.Circle[radius=" + radius + " color=" + color + "]";    }
+
+    public double getArea() {
+        return radius*radius*Math.PI;
+    }
+}
+
