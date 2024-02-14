6448e34d4fd9515f26e8aadb0ddddc0ea1b556c1
LauraMunozUAH <l.m.centenera@gmail.com>
2024-02-14 09:06:34 +0100

    Cambio

---
+++ Test/Java/es/uah/matcomp/md/el1/CircleTest.java
@@ -0,0 +1,33 @@
+package es.uah.matcomp.md.el1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircleTest {
+
+    @Test
+    void getRadius() {
+        assert
+    }
+
+    @Test
+    void getColor() {
+    }
+
+    @Test
+    void setRadius() {
+    }
+
+    @Test
+    void setColor() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void getArea() {
+    }
+}
