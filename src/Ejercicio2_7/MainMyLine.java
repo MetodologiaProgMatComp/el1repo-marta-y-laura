da728429a96f8f38364427c9e00e3d3b4861cd3c
LauraMunozUAH <l.m.centenera@gmail.com>
2024-02-24 22:09:38 +0100

    cambio pequeño en main

--- src/Ejercicio2_7/MainMyLine.java
+++ src/Ejercicio2_7/MainMyLine.java
@@ -6,26 +6,25 @@ public class MainMyLine {
         MyPoint b = new MyPoint(3,4);
         MyPoint e = new MyPoint(7,8);
         System.out.println(li);
-        li.setBegin(b); // Test setters
+        li.setBegin(b);
         li.setEnd(e);
         li.setBeginX(2);
         li.setBeginY(3);
         li.setEndX(7);
         li.setEndY(9);
-        System.out.println("El punto de inicio es: " + li.getBegin()); // Test getters
+        System.out.println("El punto de inicio es: " + li.getBegin());
         System.out.println("El punto donde acaba es: " + li.getEnd());
         System.out.println("x del punto de inicio es: " + li.getBeginX());
         System.out.println("y del punto de inicio es: " + li.getBeginY());
         System.out.println("x del punto donde acaba es: " + li.getEndX());
         System.out.println("y del punto donde acaba es: " + li.getEndY());
-        li.setBeginXY(3, 0); // Test setXY()
+        li.setBeginXY(3, 0);
         li.setEndXY(8, 9);
-        System.out.println(li.getBeginXY()[0]); // Test getXY()
+        System.out.println(li.getBeginXY()[0]);
         System.out.println(li.getBeginXY()[1]);
-        System.out.println(li.getEndXY()[0]); // Test getXY()
+        System.out.println(li.getEndXY()[0]);
         System.out.println(li.getEndXY()[1]);
         System.out.println(li);
-        //methods distance()
         System.out.println(li.getLength());
         System.out.println(li.getGradient());
     }
